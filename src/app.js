049e0bdbd8430766eac89d378c07c8fe840b0bb8
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2022-05-02 10:42:06 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 var express = require('express');
 app = express();
 
+console.log("----------- DATOS DE LAS VARIABLES --------------");
+console.log('HOST::::::', process.env.HOST);
+console.log('DATABASE:::::::::::::', process.env.DATABASE);
+console.log('USERNAME::::::', process.env.USER);
+console.log('PASSWORD::::::', process.env.PASSWORD);
+console.log("----------- DATOS DE LAS VARIABLES --------------");
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
-  res.send('-----------------');
-  res.send('HOST=${process.env.HOST}');
-  res.send('DATABASE=${process.env.DATABASE}');
-  res.send('USER=${process.env.USER}');
-  res.send('PASSWORD=${process.env.PASSWORD}');
+  res.send(process.env.HOST);
+  res.send(process.env.DATABASE);
+  res.send(process.env.USER);
+  res.send(process.env.PASSWORD); 
 });
 
 app.listen(8080, function () {
