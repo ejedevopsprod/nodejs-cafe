8191d8a35e6fa82458f57b4176b0013cc7013852
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2022-05-02 10:18:54 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
   res.send('-----------------');
-  res.send(('HOST=${process.env.HOST}');
+  res.send('HOST=${process.env.HOST}');
   res.send('DATABASE=${process.env.DATABASE}');
   res.send('USER=${process.env.USER}');
   res.send('PASSWORD=${process.env.PASSWORD}');
