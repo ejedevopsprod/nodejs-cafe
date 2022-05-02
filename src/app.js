9167a3cfc91c7c14bb1e19293741bb64b1260b49
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2022-05-02 10:11:13 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
 var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+  res.send('-----------------');
+  res.send(('HOST=${process.env.HOST}');
+  res.send('DATABASE=${process.env.DATABASE}');
+  res.send('USER=${process.env.USER}');
+  res.send('PASSWORD=${process.env.PASSWORD}');
 });
 
 app.listen(8080, function () {
